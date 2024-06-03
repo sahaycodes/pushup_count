2d122258b02b8e2f7951d6f2389d00f576b639ff
sahaycodes <sahay281620@gmail.com>
2024-06-03 21:12:18 +0530

    made code changes

--- app.py
+++ app.py
@@ -152,4 +152,4 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host='0.0.0.0', port=5000,debug=True)
